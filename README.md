90d3c9d889bc8cf97c4f9fbe0432c317655fb0c9
Artur M. <artyrillo@yandex.ru>
2023-10-25 21:18:39 +0300

    clear functions

---
+++ README.md
@@ -0,0 +1,1 @@
+# Exercises from "Grokking functional programming"
--- src/main/scala/TipCalculator.scala
+++ src/main/scala/TipCalculator.scala
@@ -11,7 +11,7 @@ object TipCalculator {
     assert(getTipPercentage(List.empty) == 0)
   }
 
-  def getTipPercentage(names: List[String]): Int = {
+  private def getTipPercentage(names: List[String]): Int = {
     if (names.size > 5) 20
     else if (names.size > 0) 10
     else 0
