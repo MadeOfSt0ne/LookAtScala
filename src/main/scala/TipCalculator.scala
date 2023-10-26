9eb442e0388180dc6caf33983596021b3e9ff445
Artur M. <artyrillo@yandex.ru>
2023-10-26 15:03:23 +0300

    clear functions

--- src/main/scala/TipCalculator.scala
+++ src/main/scala/TipCalculator.scala
@@ -1,7 +1,7 @@
 package org.example
 
 object TipCalculator {
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
     println("Empty list = " + TipCalculator.getTipPercentage(List.empty))
     println("List of 3 = " + TipCalculator.getTipPercentage(List("Alice", "Bob", "Charlie")))
     println("List of 6 = " + TipCalculator.getTipPercentage(List("Alice", "Bob", "Charlie", "Alice", "Bob", "Charlie")))
